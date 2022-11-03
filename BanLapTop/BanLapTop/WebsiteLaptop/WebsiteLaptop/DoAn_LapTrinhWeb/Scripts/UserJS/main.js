649e3e62aaeda8c900f6c016ef330389bfae2e6a
Phan Định <81685801+phanvandinh99@users.noreply.github.com>
2022-11-03 23:37:36 +0700

    UPD: DATABASE + CODE

--- BanLapTop/BanLapTop/WebsiteLaptop/WebsiteLaptop/DoAn_LapTrinhWeb/Scripts/UserJS/main.js
+++ BanLapTop/BanLapTop/WebsiteLaptop/WebsiteLaptop/DoAn_LapTrinhWeb/Scripts/UserJS/main.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
     "use strict";
 
     /*****************************
@@ -10,7 +10,7 @@
     /****************************
      * Sticky Menu
      *****************************/
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var scroll = $(window).scrollTop();
         if (scroll < 100) {
             $(".sticky-header").removeClass("sticky");
@@ -19,7 +19,7 @@
         }
     });
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var scroll = $(window).scrollTop();
         if (scroll < 100) {
             $(".seperate-sticky-bar").removeClass("sticky");
@@ -31,14 +31,14 @@
     /************************************************
      * Modal Search 
      ***********************************************/
-    $('a[href="#search"]').on('click', function(event) {
+    $('a[href="#search"]').on('click', function (event) {
         event.preventDefault();
         $('#search').addClass('open');
         $('#search > form > input[type="search"]').focus();
     });
 
-    $('#search, #search button.close').on('click', function(event) {
-        if ( event.target.className == 'close' ) {
+    $('#search, #search button.close').on('click', function (event) {
+        if (event.target.className == 'close') {
             $(this).removeClass('open');
         }
     });
@@ -46,12 +46,12 @@
     /*****************************
      * Off Canvas Function
      *****************************/
-    (function() {
+    (function () {
         var $offCanvasToggle = $('.offcanvas-toggle'),
             $offCanvas = $('.offcanvas'),
             $offCanvasOverlay = $('.offcanvas-overlay'),
             $mobileMenuToggle = $('.mobile-menu-toggle');
-        $offCanvasToggle.on('click', function(e) {
+        $offCanvasToggle.on('click', function (e) {
             e.preventDefault();
             var $this = $(this),
                 $target = $this.attr('href');
@@ -62,7 +62,7 @@
                 $this.addClass('close');
             }
         });
-        $('.offcanvas-close, .offcanvas-overlay').on('click', function(e) {
+        $('.offcanvas-close, .offcanvas-overlay').on('click', function (e) {
             e.preventDefault();
             $body.removeClass('offcanvas-open');
             $offCanvas.removeClass('offcanvas-open');
@@ -83,7 +83,7 @@
         $offCanvasNavSubMenu.parent().prepend('<div class="offcanvas-menu-expand"></div>');
 
         /*Category Sub Menu Toggle*/
-        $offCanvasNav.on('click', 'li a, .offcanvas-menu-expand', function(e) {
+        $offCanvasNav.on('click', 'li a, .offcanvas-menu-expand', function (e) {
             var $this = $(this);
             if ($this.attr('href') === '#' || $this.hasClass('offcanvas-menu-expand')) {
                 e.preventDefault();
@@ -392,16 +392,16 @@
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-      });
+    });
 
-      var modalGalleryTop = new Swiper('.modal-product-image-large', { 
+    var modalGalleryTop = new Swiper('.modal-product-image-large', {
         thumbs: {
-          swiper: modalGalleryThumbs
+            swiper: modalGalleryThumbs
         }
-      });
+    });
 
     /********************************
      * Blog List Slider - Single Slide
@@ -431,7 +431,7 @@
         min: 0,
         max: 500,
         values: [75, 300],
-        slide: function(event, ui) {
+        slide: function (event, ui) {
             $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
         }
     });
@@ -444,17 +444,17 @@
      * Animate on Scroll
      ***********************************************/
     AOS.init({
-       
-        duration: 1000, 
-        once: true, 
+
+        duration: 1000,
+        once: true,
         easing: 'ease',
     });
-    window.addEventListener('load', AOS.refresh);    
+    window.addEventListener('load', AOS.refresh);
 
     /************************************************
      * Video  Popup
      ***********************************************/
-    $('.video-play-btn').venobox(); 
+    $('.video-play-btn').venobox();
 
     /************************************************
      * Scroll Top
@@ -594,3 +594,7 @@
     });
 
 })(jQuery);
+function FunctionMessage() {
+    alert("Chuc Nang Chua Duoc Phat Trien");
+    return false;
+}
